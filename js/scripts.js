87a6f6b7d43d13645383323ea20e038889558688
Alex Altieri & Vaughn Dice <student@epicodus.com>
2015-09-09 10:25:52 -0700

    Handle words that start with 'qu' or 'Y'

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,18 @@
 var pigLatin = function(message) {
-  var vowels = ['a', 'e', 'i', 'o', 'u'];
+  var vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
   var splitMessage = message.split("");
   var consonants = '';
 
   var index = vowels.indexOf(splitMessage[0].toLowerCase());
+  if (splitMessage[0].toLowerCase() === 'y') {
+    consonants += splitMessage.shift();
+  }
   while (index === -1) {
     consonants += splitMessage.shift();
     index = vowels.indexOf(splitMessage[0].toLowerCase());
+  }
+  if (splitMessage[0] === 'u') {
+    consonants += splitMessage.shift();
   }
   var translation = splitMessage.join('') + consonants + 'ay';
   return translation;
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,27 @@
 describe("pigLatin", function() {
   it('adds an ay to the end of a word that starts with a vowel', function() {
     expect(pigLatin("Apple")).to.equal("Appleay");
+    expect(pigLatin("Ukraine")).to.equal("Ukraineay")
   });
 
   it('moves all leading consonants to end of string and adds \'ay\' ', function() {
     expect(pigLatin("Grapefruit")).to.equal("apefruitGray")
   });
+
+  it('moves three leading consonants to end of string and adds \'ay\' ', function() {
+    expect(pigLatin("Stringbeans")).to.equal("ingbeansStray")
+  });
+
+  it('moves \'qu\' together if at beginning of word', function() {
+    expect(pigLatin("Quasar")).to.equal("asarQuay")
+  });
+
+  it('treats \'y\' as a consonant if at beginning', function() {
+    expect(pigLatin("Yellow")).to.equal("ellowYay")
+  });
+
+  it('treats \'y\' as a vowel if in middle', function() {
+    expect(pigLatin("Skydive")).to.equal("ydiveSkay")
+  });
+
 });
